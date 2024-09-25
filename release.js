6fb20f4fe66efbf5cbb51b8f241db1ae77f1b3da
Majo Cuprik <marian.cuprik@icloud.com>
2024-09-25 11:40:16 +0200

    Release v0.0.43
    # Release Notes
    
    ## Changes in this version:
    
    ## Additional Notes
    - Please report any issues or bugs you encounter
    - For more details, see the full changelog below
    
    Changelog:
    - Bump version in package.json to v0.0.42

--- release.js
+++ release.js
@@ -1,19 +1,24 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
+const { execSync } = require("child_process");
+const fs = require("fs");
 
 try {
   // Get the new version
-  const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+  const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
   const newVersion = packageJson.version;
 
   // Generate changelog
-  const changelog = execSync('git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"- %s"').toString();
+  const changelog = execSync(
+    'git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"- %s"',
+  ).toString();
 
   // Create GitHub release
-  execSync(`gh release create v${newVersion} -t "Release ${newVersion}" -n "${changelog}"`, { stdio: 'inherit' });
+  execSync(
+    `gh release create v${newVersion} -t "Release ${newVersion}" -n "${changelog}"`,
+    { stdio: "inherit" },
+  );
 
   console.log(`Release v${newVersion} created successfully!`);
 } catch (error) {
-  console.error('Error creating release:', error);
+  console.error("Error creating release:", error);
   process.exit(1);
 }
